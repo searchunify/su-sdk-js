13aa0fd4f170fcbb519233e573f99b0ff35db08c
prerna <prerna.bhatia@grazitti.com>
2025-04-23 11:24:47 +0530

    correct joi validator

--- src/validations/client-validations.js
+++ src/validations/client-validations.js
@@ -1,23 +1,41 @@
 const Joi = require('joi');
 
-const initialize = Joi.object().keys({
+const initialize = Joi.object({
   instance: Joi.string().uri().trim().required(),
   timeout: Joi.number().min(30000).max(180000),
   apiKey: Joi.string(),
-  authType: Joi.string(),
-  oauth2: Joi.object().keys({
-    clientId: Joi.string().required(),
-    clientSecret: Joi.string().required(),
-    username: Joi.string().when(Joi.ref('/authType'), {
-      is: 'oauth2',
-      then: Joi.required(),
-      otherwise: Joi.forbidden()
-    }),
-    password: Joi.string().when(Joi.ref('/authType'), {
-      is: 'oauth2',
-      then: Joi.required(),
-      otherwise: Joi.forbidden()
-    })
+  authType: Joi.string().valid('apiKey', 'oauth2', 'clientCredentials'),
+  oauth2: Joi.when('authType', {
+    switch: [
+      {
+        is: 'apiKey',
+        then: Joi.forbidden()
+      },
+      {
+        is: 'clientCredentials',
+        then: Joi.object({
+          clientId: Joi.string().required(),
+          clientSecret: Joi.string().required(),
+          username: Joi.forbidden(),
+          password: Joi.forbidden()
+        }).required()
+      },
+      {
+        is: Joi.valid('oauth2', Joi.valid(null)), // fallback to oauth2-like behavior
+        then: Joi.object({
+          clientId: Joi.string().required(),
+          clientSecret: Joi.string().required(),
+          username: Joi.string().required(),
+          password: Joi.string().required()
+        }).required()
+      }
+    ],
+    otherwise: Joi.object({
+      clientId: Joi.string().required(),
+      clientSecret: Joi.string().required(),
+      username: Joi.string().required(),
+      password: Joi.string().required()
+    }).required()
   })
 }).or('apiKey', 'oauth2');
 
